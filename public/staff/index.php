9e6309fb2504ccf873f157f145954743979cd023
Jose Alvarez Pulido <jlalvarezpulido@gmail.com>
2024-04-18 22:29:02 -0700

    J and L Home website init

---
+++ public/staff/index.php
@@ -0,0 +1,26 @@
+<?php require_once('../../private/initialize.php'); ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head> 
+    <title>J&L Home</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" media="all" href="../stylesheets/staff.css" />
+  </head>
+<body>
+  <header>
+    <h1>J&L staff area</h1>
+  </header>
+  <navigation>
+    <ul>
+      <li><a href="index.php">Menu</a> </li>
+    </ul>
+  </navigation>
+  <div id="content">
+    
+  </div>
+  <footer>
+    &copy; <?php echo date('Y'); ?> J&L Home
+  </footer>
+</body>
+</html>
+
