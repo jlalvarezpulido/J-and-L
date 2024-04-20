ab4c191d1d8d5d468c77c0eb22f0add50e185b51
Jose Alvarez Pulido <jlalvarezpulido@gmail.com>
2024-04-20 13:13:30 -0700

    added php page for subject route

---
+++ public/staff/subjects/index.php
@@ -0,0 +1,7 @@
+<?php require_once('../../../private/initialize.php'); ?>
+<?php $page_title = 'J and L Kitchen'; ?>
+<?php include('../../../private/shared/staff_header.php'); ?>
+  <div id="content">
+    
+  </div>
+<?php include('../../../private/shared/staff_footer.php'); ?>
