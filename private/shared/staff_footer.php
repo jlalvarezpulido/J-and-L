eb7ec2f4b464a4b1e4490aa9a89f54c9de9e2205
Jose Alvarez Pulido <jlalvarezpulido@gmail.com>
2024-04-20 13:06:27 -0700

    restructured the directories

---
+++ private/shared/staff_footer.php
@@ -0,0 +1,6 @@
+  <footer>
+    &copy; <?php echo date('Y'); ?> J&L Home
+  </footer>
+</body>
+</html>
+
---
+++ private/shared/staff_header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head> 
+    <title>J&L Home</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" media="all" href="../public/stylesheets/staff.css" />
+  </head>
+<body>
+  <header>
+    <h1>J&L staff area</h1>
+  </header>
+  <navigation>
+    <ul>
+      <li> test </li>
+    </ul>
+  </navigation>
